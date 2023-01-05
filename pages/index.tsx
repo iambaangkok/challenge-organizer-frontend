1667df0cc7a6034049ccc39dea1ef1d7c7f59a7e
DarkTXYZ <pawaretmontfort@gmail.com>
2023-01-06 00:39:13 +0700

    fix tailwindCSS bug , test Link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
 import { Button, Grid, Stack } from "@mui/material";
 
 import { Delete } from '@mui/icons-material'
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div></div>
+    <>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <div>This is home page</div>
+      <Link href='\login' className='no-underline'>
+        <Button variant='contained'>
+          Go to Login page
+        </Button>
+      </Link>
+    </>
   );
 }
