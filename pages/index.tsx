3cace85a19a04f1960f0779f49654ae68996bc23
DarkTXYZ <pawaretmontfort@gmail.com>
2023-01-08 16:09:13 +0700

    create Challenge Card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Button, Grid, Stack } from "@mui/material";
 
 import { Delete } from '@mui/icons-material'
 import Link from 'next/link';
-
+import ChallengeCard from '../components/homepage/ChallengeCard';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -13,6 +13,7 @@ export default function Home() {
     <>
       <Head>
         <title>Login Page</title>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300&display=swap" rel="stylesheet" />
       </Head>
       <div>This is home page</div>
       <Link href='\login' className='no-underline'>
@@ -20,6 +21,10 @@ export default function Home() {
           Go to Login page
         </Button>
       </Link>
+
+      <div className='my-10'></div>
+
+      <ChallengeCard></ChallengeCard>
     </>
   );
 }
