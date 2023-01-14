221513f22ddd284639b42c25b7fe032f9bc871b5
DarkTXYZ <pawaretmontfort@gmail.com>
2023-01-14 21:55:55 +0700

    revert pages (profile page doesn't belong to GEB-130 branch)

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -14,21 +14,27 @@ export default function NavBar() {
     const [notiNum, setNotiNum] = useState<number>(1)
 
     return (
-        <div className={styles.NavBar + ' ShadowContainer'}>
-            <div className={styles.Left}>
-                <div className={styles.Brand + ' H1'}>CRINGE.IO</div>
+        <div className={styles.NavBar + ' flex justify-between ShadowContainer '}>
+            <div className={styles.Left + ' h-full flex items-center'}>
+                <div className={styles.Brand + ' H1 h-full flex flex-col justify-center'}>CRINGE.IO</div>
                 {
                     navLinks.map((link, index) => {
                         if (router.asPath.includes(link.path))
                             return (
-                                <Link id={link.name} className={styles.NavElementUnderline + ' TextBold'} href={link.path} key={index}>
-                                    {link.name}
+                                <Link href={link.path} className=' text-white h-full flex flex-col justify-center px-4 hover:bg-orange-500 ease-out duration-150'>
+
+                                    <div id={link.name} className='TextBold flex flex-col justify-center' key={index}>
+                                        {link.name}
+                                    </div>
                                 </Link>
+
                             )
                         else
                             return (
-                                <Link id={link.name} className={styles.NavElement + ' TextBold'} href={link.path} key={index}>
-                                    {link.name}
+                                <Link href={link.path} className='text-white h-full flex flex-col justify-center px-4 no-underline hover:bg-orange-500 ease-out duration-150'>
+                                    <div id={link.name} className='TextBold' key={index}>
+                                        {link.name}
+                                    </div>
                                 </Link>
                             )
                     })
@@ -38,15 +44,15 @@ export default function NavBar() {
                 {
                     notiNum > 0 ?
                         <Badge badgeContent={notiNum} color="error">
-                            <NotificationsIcon color="action" sx = {{
-                                fontSize: 26 ,
-                                color : 'White'
-                            }}/>
+                            <NotificationsIcon color="action" sx={{
+                                fontSize: 26,
+                                color: 'White'
+                            }} />
                         </Badge>
                         :
-                        <NotificationsIcon color="action" sx = {{
-                            fontSize: 26 ,
-                            color : 'White'
+                        <NotificationsIcon color="action" sx={{
+                            fontSize: 26,
+                            color: 'White'
                         }} />
                 }
 
