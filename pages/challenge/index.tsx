3a9754b55b8b0c2f0aeb6cfc4eb8b44aeb42cb54
Baangkok Vanijyananda <iambaangkok@gmail.com>
2023-01-19 11:44:51 +0700

    create challenge page template

---
+++ pages/challenge/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import ChallengeDashboard from '../../components/homepage/ChallengeDashboard';
+import TaskDashboard from '../../components/homepage/TaskDashboard';
+
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Challenge</title>
+      </Head>
+      <div className='flex justify-center items-start space-x-4 mt-6'>
+
+      </div>
+    </>
+  );
+}
