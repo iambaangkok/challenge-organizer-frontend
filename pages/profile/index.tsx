c0798e2e542d4dc267ad78485fc1a754e7a5ba01
DarkTXYZ <pawaretmontfort@gmail.com>
2023-01-26 14:24:46 +0700

    Add title to Profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,15 +2,17 @@ import Achievement from "../../components/profile/Achievement";
 import InfoDashboard from "../../components/profile/InfoDashboard";
 import Inventory from "../../components/profile/Inventory";
 import styles from './index.module.css'
+import Head from 'next/head'
 
 export default function Profile() {
     return (
         <>
-            <div className='flex mt-8 space-x-4 justify-center'>
-                <div className = {styles.Left}>
-                    <InfoDashboard />
-                </div>
-                <div className= {styles.Right + ' flex flex-col space-y-4'} >
+            <Head>
+                <title>Profile</title>
+            </Head>
+            <div className='flex w-full mt-8 space-x-4 justify-center'>
+                <InfoDashboard />
+                <div className={styles.Right + ' flex flex-col justify-between space-y-4'} >
                     <Achievement />
                     <Inventory />
                 </div>
