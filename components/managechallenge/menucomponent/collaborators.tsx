6e420ffbcca2a2c81479f35dd8401c0e45ac1b6e
majespectre00 <76035408+majespectre00@users.noreply.github.com>
2023-03-11 23:13:56 +0700

    "add" functinoality

--- components/managechallenge/menucomponent/collaborators.tsx
+++ components/managechallenge/menucomponent/collaborators.tsx
@@ -1,6 +1,7 @@
-import { addCollab, ManageChalStore } from '../store/managechalStore';
+import {TextField } from '@mui/material';
 import collabStyle from '../css/Collabs.module.css';
 import { useState } from 'react';
+import styles from '../../challengecreation/css/CreationPage.module.css'
 import { setConstantValue } from 'typescript';
 type Collaborator = {
     cnumber: number;
@@ -23,45 +24,69 @@ export default function Collaborators() {
     const removeCol = (cc: Collaborator) => {
         setCols(cols.filter((e) => e !== cc));
     };
+    const [emailInput,setEmailInput] = useState("");
     const addCol = (c: Collaborator) => {
         if (!cols.includes(c)) setCols([...cols, c]);
+        console.log(emailInput)
+        setEmailInput("")
     };
     return (
-        <div className={collabStyle.tbox + ' w-full'}>
-            <div className="w-full">
-                <button onClick={() => addCol(c)}>add</button>
+        <div className="w-full pb-2">
+            <div className={styles.cr_HeadText + ' pb-2'}>
+                Collaborators
             </div>
-            <div className={collabStyle.ttable + ' w-full'}>
-                <div className={collabStyle.thead }>
-                    <div className={collabStyle.theadtext}>#</div>
-                    <div className={collabStyle.theadtext}>Participant ID</div>
-                    <div className={collabStyle.theadtext}>
-                        Participant Name
+            <div className={collabStyle.tbox + ' w-full'}>
+                <div className="w-full flex flex-row gap-1">
+                    <div className = "w-11/12">
+
+                    <TextField
+                        onChange = {(e) =>{
+                            setEmailInput(e.target.value)
+                        }}
+                        placeholder = "Please input your collaborator's email."
+                        fullWidth
+                        value = {emailInput}
+                    ></TextField>
                     </div>
-                    <div className={collabStyle.theadtext}>Action</div>
+                   
+                    <button className = "" onClick={() => addCol(c)}>add</button>
+
+             
                 </div>
-                {cols.length > 0 &&
-                    cols.map((co: Collaborator, index) => {
-                        return (
-                            <div className={collabStyle.tbody} key={index}>
-                                <div className={collabStyle.tbodytext}>
-                                    {index + 1}
+                <div className={collabStyle.ttable + ' w-full'}>
+                    <div className={collabStyle.thead}>
+                        <div className={collabStyle.theadtext}>#</div>
+                        <div className={collabStyle.theadtext}>
+                            Participant ID
+                        </div>
+                        <div className={collabStyle.theadtext}>
+                            Participant Name
+                        </div>
+                        <div className={collabStyle.theadtext}>Action</div>
+                    </div>
+                    {cols.length > 0 &&
+                        cols.map((co: Collaborator, index) => {
+                            return (
+                                <div className={collabStyle.tbody} key={index}>
+                                    <div className={collabStyle.tbodytext}>
+                                        {index + 1}
+                                    </div>
+                                    <div className={collabStyle.tbodytext}>
+                                        {co.id}
+                                    </div>
+                                    <div className={collabStyle.tbodytext}>
+                                        {co.name}
+                                    </div>
+                                    <button
+                                        className=""
+                                        onClick={() => removeCol(co)}
+                                    >
+                                        remove
+                                    </button>
                                 </div>
-                                <div className={collabStyle.tbodytext}>
-                                    {co.id}
-                                </div>
-                                <div className={collabStyle.tbodytext}>
-                                    {co.name}
-                                </div>
-                                <button
-                                    className=""
-                                    onClick={() => removeCol(co)}
-                                >
-                                    remove
-                                </button>
-                            </div>
-                        );
-                    })}
+                            );
+                        })}
+                </div>
             </div>
         </div>
     );
