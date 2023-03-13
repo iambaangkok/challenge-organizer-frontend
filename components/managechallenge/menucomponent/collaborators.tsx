90f37601ece576dcad63a35303b11ba710eefd19
majespectre00 <76035408+majespectre00@users.noreply.github.com>
2023-03-13 15:12:32 +0700

    pritter

--- components/managechallenge/menucomponent/collaborators.tsx
+++ components/managechallenge/menucomponent/collaborators.tsx
@@ -1,7 +1,7 @@
-import {TextField } from '@mui/material';
+import { TextField } from '@mui/material';
 import collabStyle from '../css/Collabs.module.css';
 import { useState } from 'react';
-import styles from '../../challengecreation/css/CreationPage.module.css'
+import styles from '../../challengecreation/css/CreationPage.module.css';
 import { setConstantValue } from 'typescript';
 import axios from 'axios';
 import router from 'next/router';
@@ -22,18 +22,20 @@ const c2 = {
     name: 'oat699',
 };
 
-
-export default function Collaborators(title:String, collaborators:UserData[]) {
+export default function Collaborators(
+    title: String,
+    collaborators: UserData[],
+) {
     // const {challengeTitle} = router.query
 
     const [cols, setCols] = useState<UserData[]>([]);
     // const [text,setText]  = useState("")
-    const [apiResp,setApiResp] = useState<Collaborator>();
+    const [apiResp, setApiResp] = useState<Collaborator>();
     const removeCol = (cc: UserData) => {
         // let.
         // axios.delete('http://localhost:3030/api/challenges//deleteCollaborators')
         // .then =>{
-            
+
         //     removeCol
         // }
         setCols(cols.filter((e) => e.userId !== cc.userId));
@@ -45,59 +47,53 @@ export default function Collaborators(title:String, collaborators:UserData[]) {
     //    })
 
     // },[])
-    const [emailInput,setEmailInput] = useState("");
+    const [emailInput, setEmailInput] = useState('');
     const addCol = (c: UserData) => {
         setCols([...cols, c]);
-        setEmailInput("")
-        console.log(emailInput)
+        setEmailInput('');
+        console.log(emailInput);
     };
     // const handleChange = (e:any) =>{
     //     setText(e.target.value.toString())
     // }
 
-    const handleAdd = () =>{
-        //send {text} to api then api return an entity
-        //call addCol  with the returned entity as param
-        let dupe = cols.map((c) => c.cmuAccount).includes(emailInput)    
-        if(!dupe) {
-        let j = {
-            challengeTitle: title,
-            cmuAccount: emailInput
+    const handleAdd = () => {
+        let dupe = cols.map((c) => c.cmuAccount).includes(emailInput);
+        if (!dupe) {
+            let j = {
+                challengeTitle: title,
+                cmuAccount: emailInput,
+            };
+            axios
+                .put('http://localhost:3030/api/challenges/addCollaborators', j)
+                .then((resp) => {
+                    console.log(resp.data.Collaborator);
+                    addCol(resp.data.Collaborator);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         }
-        axios
-            .put('http://localhost:3030/api/challenges/addCollaborators',j)
-            .then((resp) =>{
-                console.log(resp.data.Collaborator)
-                // addCol(resp.data.Collaborator)
-            })
-            .catch((err) =>{
-                console.log(err)
-            })
-        }   
-    }
+    };
     return (
         <div className="w-full pb-2">
-            <div className={styles.cr_HeadText + ' pb-2'}>
-                Collaborators
-            </div>
+            <div className={styles.cr_HeadText + ' pb-2'}>Collaborators</div>
             <div className={collabStyle.tbox + ' w-full'}>
                 <div className="w-full flex flex-row gap-1">
-                    <div className = "w-11/12">
-
-                    <TextField
-                        onChange = {(e) =>{
-                            setEmailInput(e.target.value)
-                            
-                        }}
-                        placeholder = "Please input your collaborator's email."
-                        fullWidth
-                        value = {emailInput}
-                    ></TextField>
+                    <div className="w-11/12">
+                        <TextField
+                            onChange={(e) => {
+                                setEmailInput(e.target.value);
+                            }}
+                            placeholder="Please input your collaborator's email."
+                            fullWidth
+                            value={emailInput}
+                        ></TextField>
                     </div>
-                   
-                    <button className = "" onClick={() => handleAdd()}>add</button>
 
-             
+                    <button className="" onClick={() => handleAdd()}>
+                        add
+                    </button>
                 </div>
                 <div className={collabStyle.ttable + ' w-full'}>
                     <div className={collabStyle.thead}>
